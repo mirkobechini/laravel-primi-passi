e7274a52dc7a8ae1e672073dc7bcd27763dcfae8
mirkobechini <mirkobechini@gmail.com>
2025-12-21 21:28:02 +0100

    Refactor home view layout and enhance navigation structure

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Primi passi</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            background-color: black;
+            color: white;
+        }
+    </style>
+
 </head>
+
 <body>
-    <h1>Hello world</h1>
+
+    <header>
+        <nav class="navbar navbar-expand-md bg-dark ">
+            <div class="container">
+                <a class="navbar-brand text-light" href="#">Laravel</a>
+                <div class="collapse navbar-collapse" >
+                    <div class="navbar-nav ">
+                        <a class="nav-link text-light" aria-current="page" href="#">Home</a>
+                        <a class="nav-link text-light" href="#">About us</a>
+                        <a class="nav-link text-light" href="#">Contacts</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="container my-3">
+            <h1>Hello world</h1>
+            <div class="p-5 mb-4 bg-success rounded-3">
+                <div class="container-fluid py-5">
+                    <h1 class="display-5 fw-bold">First page using Laravel</h1>
+                    <p class="col-md-8 fs-4">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, nostrum.
+                    </p>
+                    <button class="btn btn-primary btn-lg" type="button">Let's try</button>
+                </div>
+            </div>
+        </div>
+
+    </main>
+    <footer>
+        <div class="bg-dark-subtle">
+            <div class="container text-white">
+                <p>Esercizio di Mirko Bechini</p>
+            </div>
+        </div>
+    </footer>
 </body>
+
 </html>
